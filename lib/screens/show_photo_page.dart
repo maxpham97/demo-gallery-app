ac59decbd905e205aff1b694ca75cb48187abacc
max <maxpham97@gmail.com>
2022-11-29 21:01:40 +0700

    add show photo from gallery page

---
+++ lib/screens/show_photo_page.dart
@@ -0,0 +1,131 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
+
+class ShowGallery extends StatefulWidget {
+  const ShowGallery({Key? key}) : super(key: key);
+
+  @override
+  State<ShowGallery> createState() => _ShowGalleryState();
+}
+
+class _ShowGalleryState extends State<ShowGallery> {
+  List<AssetEntity> assets = [];
+
+  _fetchAssets() async {
+    final albums = await PhotoManager.getAssetPathList(onlyAll: true);
+    final recentAlbum = albums.first;
+
+    final recentAssets = await recentAlbum.getAssetListRange(
+      start: 0,
+      end: 1000000,
+    );
+    setState(() => assets = recentAssets);
+  }
+
+  @override
+  void initState() {
+    _fetchAssets();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gallery'),
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          // A grid view with 3 items per row
+          crossAxisCount: 3,
+        ),
+        itemCount: assets.length,
+        itemBuilder: (_, index) {
+          return AssetThumbnail(asset: assets[index]);
+        },
+      ),
+    );
+  }
+}
+
+class AssetThumbnail extends StatelessWidget {
+  const AssetThumbnail({
+    Key? key,
+    required this.asset,
+  }) : super(key: key);
+
+  final AssetEntity asset;
+
+  @override
+  Widget build(BuildContext context) {
+    // future: asset.thumbData,
+    return FutureBuilder<Uint8List>(
+      // future: asset.thumbData,
+      builder: (_, snapshot) {
+        final bytes = snapshot.data;
+        // If we have no data, display a spinner
+        if (bytes == null) return CircularProgressIndicator();
+        return InkWell(
+          onTap: () {
+            if (asset.type == AssetType.image) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => Text("1"),
+                ),
+              );
+            }
+            // TODO: navigate to the video screen
+          },
+          child: Stack(
+            children: [
+              // Wrap the image in a Positioned.fill to fill the space
+              Positioned.fill(
+                child: Image.memory(bytes, fit: BoxFit.cover),
+              ),
+              // Display a Play icon if the asset is a video
+              if (asset.type == AssetType.video)
+                Center(
+                  child: Container(
+                    color: Colors.blue,
+                    child: const Icon(
+                      Icons.play_arrow,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class ImageScreen extends StatelessWidget {
+  const ImageScreen({
+    Key? key,
+    required this.imageFile,
+  }) : super(key: key);
+
+  final Future<File> imageFile;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      alignment: Alignment.center,
+      child: FutureBuilder<File>(
+        future: imageFile,
+        builder: (_, snapshot) {
+          final file = snapshot.data;
+          if (file == null) return Container();
+          return Image.file(file);
+        },
+      ),
+    );
+  }
+}
