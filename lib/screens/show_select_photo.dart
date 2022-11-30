06ce0acdfe0e0075c7ea11169098a7aa03aa9ddf
max <maxpham97@gmail.com>
2022-11-30 21:33:31 +0700

    add show pick photo

---
+++ lib/screens/show_select_photo.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class SelectPhotoPage extends StatefulWidget {
+  const SelectPhotoPage({Key? key}) : super(key: key);
+
+  @override
+  State<SelectPhotoPage> createState() => _SelectPhotoPageState();
+}
+
+class _SelectPhotoPageState extends State<SelectPhotoPage> {
+  final ImagePicker imagePicker = ImagePicker();
+  List<XFile>? imageFileList = [];
+
+  void selectImages() async {
+    final List<XFile>? selectedImages = await imagePicker.pickMultiImage();
+    if (selectedImages!.isNotEmpty) {
+      imageFileList!.addAll(selectedImages);
+    }
+    print("Image List Length:" + imageFileList!.length.toString());
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Multiple Images'),
+        ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  selectImages();
+                },
+                child: Text('Select Images'),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GridView.builder(
+                      itemCount: imageFileList!.length,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3),
+                      itemBuilder: (BuildContext context, int index) {
+                        return Image.file(
+                          File(imageFileList![index].path),
+                          fit: BoxFit.cover,
+                        );
+                      }),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
